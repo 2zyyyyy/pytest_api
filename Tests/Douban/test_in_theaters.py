9b1319f05a3a96c0dc0e549d8fc8cf70eabd3921
wanli <zhangyun@ifugle.com>
2021-03-16 17:33:46 +0800

    add:新增Macwk接口测试脚本及数据

---
+++ Tests/Douban/test_in_theaters.py
@@ -0,0 +1,32 @@
+import pytest
+import requests
+from Untils.commonlib import get_test_data
+import pprint
+import sys
+
+path = sys.path[0] + "/Data/Douban/test_in_theaters.yaml"
+cases, list_params = get_test_data(path)
+
+
+@pytest.fixture(scope='session')
+def preparation():
+    print("在数据库中准备测试数据")
+    test_data = "在数据库中准备测试数据"
+    yield test_data
+    print("清理测试数据")
+
+
+class TestInTheaters(object):
+
+    @pytest.mark.parametrize('case, http, expected', list(list_params), ids=cases)
+    def test_in_theaters(self, env, case, http, expected):
+        res = requests.request(http['method'],
+                               url=env['host']['douban'] + http['path'],
+                               headers=http['headers'],
+                               params=http['params'])
+        response = res.json()
+        pprint.pprint(response)
+        assert response['data'][0]['url'] == expected['response']['url']
+        assert response['data'][0]['rate'] == expected['response']['rate']
+        assert response['data'][0]['title'] == expected['response']['title'], \
+            "实际的电影是：{}".format(response['data'][0]['title'])
--- Tests/Macwk/test_in_macsofts.py
+++ Tests/Macwk/test_in_macsofts.py
@@ -2,8 +2,10 @@ import pytest
 import requests
 from Untils.commonlib import get_test_data
 import pprint
+import sys
 
-cases, list_params = get_test_data("/Users/gilbert/PycharmProjects/pytest_api/Data/test_in_theaters.yaml")
+path = sys.path[0] + "/Data/Macwk/test_in_macsofts.yaml"
+cases, list_params = get_test_data(path)
 
 
 @pytest.fixture(scope='session')
@@ -14,16 +16,16 @@ def preparation():
     print("清理测试数据")
 
 
-class TestInTheaters(object):
+class TestInMacsofts(object):
 
     @pytest.mark.parametrize('case, http, expected', list(list_params), ids=cases)
     def test_in_theaters(self, env, case, http, expected):
         res = requests.request(http['method'],
-                               url=env['host']['douban'] + http['path'],
+                               url=env['host']['macwk'] + http['path'],
                                headers=http['headers'],
                                params=http['params'])
         response = res.json()
         pprint.pprint(response)
-        assert response['data'][0]['url'] == expected['response']['url']
-        assert response['data'][0]['rate'] == expected['response']['rate']
-        assert response['data'][0]['title'] == expected['response']['title'], "实际的电影是：{}".format(response['title'])
+        assert response['data'][0]['language'][0] == expected['response']['language']
+        assert response['data'][0]['title'] == expected['response']['title'], \
+            "实际的名称是：{}".format(response['data'][0]['title'])
