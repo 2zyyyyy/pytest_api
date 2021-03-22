d80d4138a6193ff8e3380bec15dbb320bdf72ccb
wanli <zhangyun@ifugle.com>
2021-03-22 18:55:59 +0800

    add:生产allure测试报告

--- Tests/Macwk/test_in_macsofts.py
+++ Tests/Macwk/test_in_macsofts.py
@@ -11,24 +11,24 @@ cases, list_params = get_test_data(path)
 
 @allure.feature("Mac软件商店")  # allure.feature:产品需求
 class TestInMacsofts(object):
-    @allure.story("Mac商店应用查询接口测试")  # allure.store:场景
+    @allure.story("Mac商店应用查询接口测试")  # allure.store:针对产品需求的实际测试场景
+    @allure.severity("NORMAL")  # allure.severity:标记测试用例级别
     @pytest.mark.parametrize('case, http, expected', list(list_params), ids=cases)
     def test_in_macsofts(self, env, case, http, expected):
         # 步骤1：调用step函数
         login("wanli", "dingtax.cn")
         # 步骤2：step的参数打印在测试报告中
         with allure.step("准备发起请求"):
-            allure.attach("attach可以打印一些附加信息~")
-            allure.attach("请求方式：", http['method'], "请求地址：", env['host']['macwk'] + http['path'])
+            allure.attach("请求方式：", http['method'])
+            allure.attach("请求地址：", env['host']['macwk'], http['path'])
             allure.attach("请求参数：", http['params'])
         with allure.step("装载数据,发起请求"):
-            res = requests.request(http['method'],
-                                   url=env['host']['macwk'] + http['path'],
-                                   headers=http['headers'],
-                                   params=http['params'])
+            self.request = requests.request(http['method'], url=env['host']['macwk'] + http['path'],
+                                            headers=http['headers'], params=http['params'])
+            res = self.request
         with allure.step("获取返回结果"):
             response = res.json()
-            allure.attach("log 输出返回结果数据")
+            # allure.attach("log 输出返回结果数据")
             logging.info(response)
         with allure.step("断言"):
             assert response['data'][0]['language'][0] == expected['response']['language']
